751d5d62f3d393a48445b0438153dbc5b43c052c
ssz1984 <37354678+ssz1984@users.noreply.github.com>
2018-04-24 20:50:07 +0800

    changes
    4.0

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'hpl&tb'
-copyright = '2018, wu'
+copyright = '2018, wucong'
 author = 'wucong'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -91,7 +91,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
